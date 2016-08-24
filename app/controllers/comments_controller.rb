69aa3953d13563b5cf8db6acd05d121704d07e2e
Hamit YAY <hamityay@hotmail.com>
2016-08-24 23:13:08 +0300

    Added associations between devise and posts.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,11 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:name, :body))
+    @comment.user_id = current_user.id
+    @comment.save
 
     redirect_to post_path(@post)
   end
